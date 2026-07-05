0b50c1153b9bb72a09a75bf37803649d665d6b31
nabilmoiun <nnaabiill@gmail.com>
2026-07-05 11:05:29 +0600

    chat: conversation messages api

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,9 +1,11 @@
 from drf_spectacular.utils import extend_schema
 
-from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import viewsets, permissions, status
 
 from apps.chat.models import Conversation
-from apps.chat.serializers import ConversationListSerializer
+from apps.chat.serializers import ConversationListSerializer, MessageViewSerializer
 
 
 @extend_schema(tags=["chat"])
@@ -11,7 +13,7 @@ class ConversationViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = ConversationListSerializer
     queryset = Conversation.objects.order_by("last_message_at")
-    serializer_class_map = {"list": ConversationListSerializer}
+    serializer_class_map = {"list": ConversationListSerializer, "messages": MessageViewSerializer}
 
     def get_serializer_class(self):
         action = getattr(self, "action", None)
@@ -30,3 +32,10 @@ class ConversationViewSet(viewsets.ReadOnlyModelViewSet):
                 "last_message__attachments",
             )
         )
+    
+    @action(detail=True, methods=["get"], url_path="messaages")
+    def messages(self, *args, **kwargs):
+        conversation = self.get_object()
+        qs = conversation.messages.all()
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
